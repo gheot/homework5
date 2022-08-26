9a19c9b53e7652a82ecec94c6bd0396ae17e57ed
nilova <marina.nilova@yandex.ru>
2022-08-27 00:24:32 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
-    public static void main(String[] args) {
+    ;
 
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.print(++i + " ");
+        }
+        System.out.println();
+        for (; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+
+        int friday = 5;
+        for (; friday <= 31; friday = friday + 7) {
+
+            System.out.println("Сегодня пятница " + friday + "-ое число. Необходимо подготовить отчет.");
+
+
+        }
+
+        int currentYear = 2022;
+        int start = currentYear - 200;
+        int end = currentYear + 100;
+        for (int year = start; year < end; year++) {
+            if (year % 79 == 0) {
+
+                System.out.println(year);
+            }
+        }
     }
 }
+
